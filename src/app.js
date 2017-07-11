025aa5f0a1033ca8e5f78762f79518bd325dc043
soumikcv <soumiksur.me@gmail.com>
2017-07-11 21:32:38 +0530

    added spinner and logout button

--- src/app.js
+++ src/app.js
@@ -1,25 +1,55 @@
 import React , { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common'
+import { Header, Button, CardSection, Spinner } from './components/common'
 import LoginForm from './components/LoginForm'
+
 class App extends Component {
+	state={loggedIn: null};
+
 	componentWillMount() {
 		firebase.initializeApp({
-    apiKey: "AIzaSyCoVNDFDOJgrszahqipKysubtABJZXb3G8",
-    authDomain: "auth-e1749.firebaseapp.com",
-    databaseURL: "https://auth-e1749.firebaseio.com",
-    projectId: "auth-e1749",
-    storageBucket: "auth-e1749.appspot.com",
-    messagingSenderId: "805874817827"
-  });
+		    apiKey: "AIzaSyCoVNDFDOJgrszahqipKysubtABJZXb3G8",
+		    authDomain: "auth-e1749.firebaseapp.com",
+		    databaseURL: "https://auth-e1749.firebaseio.com",
+		    projectId: "auth-e1749",
+		    storageBucket: "auth-e1749.appspot.com",
+		    messagingSenderId: "805874817827"
+		  });
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if(user){
+				this.setState({loggedIn:true});
+				console.log(user);
+			}
+			else{
+				this.setState({loggedIn:false});
+			}
+
+		});
 	}
+
+	renderContent() {
+		switch(this.state.loggedIn){
+			case true:
+				return (
+				<CardSection>
+				<Button onPress={() => firebase.auth().signOut()}>Log out!</Button>
+				</CardSection>
+				);
+			case false:
+				return <LoginForm />
+			default:
+				<Spinner />
+		}
+	}
+
 	render()
 	{
 	return(
 		<View>
 			<Header header="Authentication" />
-			<LoginForm />
+			{this.renderContent()}
 		</View>
 		);
 	}
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,48 @@
 import React , { Component } from 'react';
-import { Button, Card, CardSection, Input } from './common'
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input, Spinner } from './common'
 
 class LoginForm extends Component{
-	state = { email: '', password: '' };
+	state = { email: '', password: '' , error: '', loading: false};
+	
+	onButtonPress()
+	{
+		const {email,password} = this.state;
+		
+		this.setState({error: '', loading:true});
+		
+		firebase.auth().signInWithEmailAndPassword(email,password)
+		.then(this.onLoginSuccess.bind(this))
+		.catch(() => {
+			firebase.auth().createUserWithEmailAndPassword(email,password)
+			.then(this.onLoginSuccess.bind(this))
+			.catch(this.onLoginFail.bind(this));
+		});
+	}
 
+	onLoginSuccess(){
+		this.setState({
+			email: '',
+			password: '',
+			error: '',
+			loading: false
+		});
+	}
 
+	onLoginFail(){
+		this.setState({
+			error: "Authentication Failed!!",
+			loading: false
+		});
+	}
+
+	renderButton(){
+		if(this.state.loading){
+			return <Spinner size="small" />;
+		}
+		return <Button onPress={this.onButtonPress.bind(this)}>Login!</Button>;
+	}
 	render() {
 		return (
 			<Card>
@@ -26,13 +64,23 @@ class LoginForm extends Component{
 					onChangeText={password => this.setState({ password })}
 					/>
 				</CardSection>
-
+				<Text style = {styles.errorTextStyles}>
+				{this.state.error}
+				</Text>
 				<CardSection>
-				<Button>Login!</Button>
+				{this.renderButton()}
 				</CardSection>
 			</Card>
 		);
 	}
 }
+
+const styles = {
+	errorTextStyles: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
 
 export default LoginForm;
